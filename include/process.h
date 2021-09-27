cd062ceed76d49d6495240e9c2d42fe379dd6f44
benemichel <benemichel_code@posteo.de>
2021-09-27 19:37:38 +0200

    process complete

--- include/process.h
+++ include/process.h
@@ -14,10 +14,11 @@ class Process {
   float CpuUtilization();                  // TODO: See src/process.cpp
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  bool operator<(Process & a);  // TODO: See src/process.cpp
 
   // TODO: Declare any necessary private members
  private:
+    int pid_;
 };
 
 #endif
